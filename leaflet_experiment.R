cefe5450c8d889a1bd90cbc994a743843530f5f4
LendieFollett <lendie.follett@drake.edu>
2021-04-23 07:03:35 -0500

    leaflet interactive mapping

---
+++ leaflet_experiment.R
@@ -0,0 +1,24 @@
+library(leaflet)
+pal <- colorBin(
+  palette = "viridis", domain = ia_shp_join$lasso_bin_pred,
+  bins = seq(0, max(ia_shp_join$lasso_bin_pred, na.rm = TRUE) + .01, by = .1)
+)
+leaflet(ia_shp_join) %>%
+  addTiles() %>%
+  #setView(lng = 0, lat = 30, zoom = 2) %>%
+  addPolygons(
+    fillColor = ~ pal(lasso_bin_pred),
+    color = "white",
+    fillOpacity = 0.7,
+    #label = ~labels,
+    highlight = highlightOptions(
+      color = "black",
+      bringToFront = TRUE
+    )
+  )
+
+# %>%
+  leaflet::addLegend(
+    pal = pal, values = ~PM2.5,
+    opacity = 0.7, title = "PM2.5"
+  )
