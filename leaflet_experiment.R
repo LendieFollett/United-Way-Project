c922198b8a5a6d3391424dfe8d032ac087a5d77a
LendieFollett <lendie.follett@drake.edu>
2021-04-23 09:19:37 -0500

    Update leaflet_experiment.R

--- leaflet_experiment.R
+++ leaflet_experiment.R
@@ -1,4 +1,31 @@
 library(leaflet)
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
+library(reshape2)
+library(tigris)
+library(sp)
+library(tidycensus)
+library(rgeos)#spatial
+library(DT)#datatable
+library(rmapshaper)
+
+
+acs <- read.csv("acs_predictions.csv")#read acs data with predictions
+
+
+#for merging
+acs$GEOID <- as.character(paste0(acs$GEOID, substr(acs$X, 13, 13)))
+#this is block groups w/in tracts
+ia_shp <- readRDS("ia_shp.RDS")#block_groups(state = "IA")  #TIME INTENSIVE 1st run (so each dashboard run)
+
+county_list <- readRDS("county_list.RDS")#unique(counties("Iowa")$NAME)#TIME INTENSIVE 1st run (so each dashboard run)
+county_list <- county_list[order(county_list)]
+all_counties <-   block_groups(state = 'IA', county = county_list) #also time intensive...
+
+ia_shp_join <- left_join(ia_shp, acs, by="GEOID" ) %>%
+  rmapshaper::ms_simplify( keep = 0.01, keep_shapes = TRUE)
+
 pal <- colorBin(
   palette = "viridis", domain = ia_shp_join$lasso_bin_pred,
   bins = seq(0, max(ia_shp_join$lasso_bin_pred, na.rm = TRUE) + .01, by = .1)
